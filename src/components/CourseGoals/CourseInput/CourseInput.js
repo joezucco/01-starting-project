2e021bb515a5888f1b981227b86f33fed26562bf
Joe Zucco <josephzucco@gmail.com>
2021-09-30 14:33:17 -0400

    styled button component

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,43 @@
 import React, { useState } from "react";
-
+import styled from "styled-components";
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const FormControl = styled.div`
+  {
+    margin: 0.5rem 0;
+  }
+
+  .form-control label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  .form-control input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  .form-control input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  .form-control.invalid input {
+    border-color: red;
+    background: rgb(255, 196, 196);
+  }
+
+  .form-control.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
